f487c68c3ce86554d6fe35a8741299dd9414f3a0
Melikinho <Jimmymelik10@gmail.com>
2022-06-30 15:21:34 +0200

    Add project files.

---
+++ MVCÖvning/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace MVCÖvning.Models
+{
+    public class Product
+    {
+        int id { get; set; }
+        string name { get; set; }
+        int price { get; set; }
+        DateTime Orderdate { get; set; }
+        string Category { get; set; }
+        int Count { get; set; }
+        string Description { get; set; }
+
+    }
+}
